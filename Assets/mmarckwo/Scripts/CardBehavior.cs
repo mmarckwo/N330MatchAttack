b3afa69e043a7523f128ad6bbfb41965df785ab2
Ethan Clark <scratchcat2@sbcglobal.net>
2022-04-02 17:29:39 -0400

    Code cleanup on CardBehavior, fixed animations

--- Assets/mmarckwo/Scripts/CardBehavior.cs
+++ Assets/mmarckwo/Scripts/CardBehavior.cs
@@ -7,11 +7,11 @@ public class CardBehavior : MonoBehaviour
     // test data.
     public string cardType = "fire";
     public int cardNumber = 0;
-    public bool FlipCard = false;
-    // can't flip while card is flipping.
-    private bool flipping = false;
+    public bool flipped = false;
 
     private Animation cardAnimation;
+	
+	private string currentAnimation;
 
     private void Start()
     {
@@ -20,37 +20,32 @@ public class CardBehavior : MonoBehaviour
 
     public void CardFlip()
     {
-
-        // flip the card if it is unflipped. unflip it if it is flipped.
-        if (FlipCard == false && flipping == false)
-        {
-            // card is flipped.
-            flipping = true;
-            StartCoroutine(playAnim("CardFlipAnim"));
-        } 
-        else if (FlipCard == true && flipping == false)
-        {
-            // card is unflipped.
-            flipping = true;
-            StartCoroutine(playAnim("CardUnflipAnim"));   
-        }
+		
+		if(!cardAnimation.IsPlaying(currentAnimation)){
+			
+			// flip the card if it is unflipped. unflip it if it is flipped.
+			if (flipped){
+				
+				// card is flipped.
+				playAnim("CardFlipAnim");
+				
+			}else{
+				
+				// card is unflipped.
+				playAnim("CardUnflipAnim");   
+				
+			}
+			
+			flipped = !flipped;
+			
+		}
         
     }
 
-    IEnumerator playAnim(string anim)
+    void playAnim(string anim)
     {
         cardAnimation.Play(anim);
-
-        while (cardAnimation.IsPlaying(anim))
-        {
-            yield return null;
-        }
-
-        // negate itself.
-        FlipCard = !FlipCard;
-        flipping = false;
-
-        Debug.Log("done flipping");
-        Debug.Log("Card flipped?: " + FlipCard);
+		currentAnimation = anim;
+		
     }
 }
