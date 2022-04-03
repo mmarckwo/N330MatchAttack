e88f560d1092878f003b9edf6a5ebe52ea370f04
mmarckwo <78805766+mmarckwo@users.noreply.github.com>
2022-04-03 00:18:04 -0400

    rudimentary card checking
    can check for matches, but it's not perfect.

--- Assets/mmarckwo/Scripts/GameManager.cs
+++ Assets/mmarckwo/Scripts/GameManager.cs
@@ -4,21 +4,55 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    // cards to check. will need to be adjusted for chained cards.
+    private string[] CardList = {"", ""};
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // -1 for index 0, 1 for index 1.
+    private int slotCheck = -1;
 
     public void CardChecker(string text)
     {
         Debug.Log("Type is: " + text);
+
+        if (slotCheck == -1)
+        {
+            CardList[0] = text;
+            slotCheck *= -1;
+        } 
+        else if(slotCheck == 1)
+        {
+            CardList[1] = text;
+            slotCheck *= -1;
+        }
+
+        Debug.Log(CardList[0] + " " + CardList[1]);
+        CheckMatch();
+        
+        
+    }
+
+    void CheckMatch()
+    {
+        // only run the check if cardlist has no empty strings.
+        if(CardList[0] != "" && CardList[1] != "")
+        {
+            if (CardList[0] == CardList[1])
+            {
+                Debug.Log("Cards match!");
+
+                // empty out card list.
+                CardList[0] = "";
+                CardList[1] = "";
+            }
+            else
+            {
+                Debug.Log("Cards don't match.");
+
+                // empty out card list.
+                CardList[0] = "";
+                CardList[1] = "";
+            }
+        }
+        
     }
 }
