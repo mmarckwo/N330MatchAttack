7ea733f4066a93c4b7e93523f8b2776d49f2d534
psyco doc <thunderfire222@yahoo.com>
2022-04-03 21:22:52 -0400

    matching effect code(NOT WORKING YET)

--- Assets/mmarckwo/Scripts/GameManager.cs
+++ Assets/mmarckwo/Scripts/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour
 {
     // cards to check. will need to be adjusted for chained cards.
     private string[] CardList = {"", ""};
-
+    //public ParticleSystem matchEffect;
     // -1 for index 0, 1 for index 1.
     private int slotCheck = -1;
 
@@ -18,7 +18,7 @@ public class GameManager : MonoBehaviour
         {
             CardList[0] = text;
             slotCheck *= -1;
-        } 
+        }
         else if(slotCheck == 1)
         {
             CardList[1] = text;
@@ -27,8 +27,8 @@ public class GameManager : MonoBehaviour
 
         Debug.Log(CardList[0] + " " + CardList[1]);
         CheckMatch();
-        
-        
+
+
     }
 
     void CheckMatch()
@@ -38,8 +38,10 @@ public class GameManager : MonoBehaviour
         {
             if (CardList[0] == CardList[1])
             {
-                Debug.Log("Cards match!");
 
+                Debug.Log("Cards match!");
+                ParticleSystem ps = GameObject.Find("TestP").GetComponent<ParticleSystem>();
+                //ps.play();
                 // empty out card list.
                 CardList[0] = "";
                 CardList[1] = "";
@@ -53,6 +55,13 @@ public class GameManager : MonoBehaviour
                 CardList[1] = "";
             }
         }
-        
+
     }
+    //void Particalsystemplay(){
+//if(CardList[0] != "" && CardList[1] != "")
+        //{
+          //matchEffect.play();
+
+          //}
+      //}
 }
