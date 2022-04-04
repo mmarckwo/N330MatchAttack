cc0ccf7cc1345a28b1df6dc678e1467b7a03f00d
psychodoctor <42424477+psychodoctor@users.noreply.github.com>
2022-04-04 12:28:45 -0400

    asd

--- Assets/mmarckwo/Scripts/GameManager.cs
+++ Assets/mmarckwo/Scripts/GameManager.cs
@@ -4,138 +4,139 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
+  public ParticleSystem ps1;
 	public CardState[] flippedCards = {null,null};
 	int numberOfFlippedCards = 0;
-	
+
 	//todo: we should have a discussion about this.
 	public CardManager cardManager;
-	
+
     public void AddFlippedCard(CardState cardState)
     {
-        
+
 		flippedCards[numberOfFlippedCards] = cardState;
-		
+
 		numberOfFlippedCards++;
-		
+
         CheckMatch();
-		
+
     }
-	
+
 	public void UnflipAllCards(){
-		
+
 		for(int i = 0; i < numberOfFlippedCards; i++){
-			
+
 			if(!flippedCards[i].matched){
-				
+
 				flippedCards[i].cardBehavior.PlayAnim("CardUnflipAnim");
 				flippedCards[i].cardBehavior.flipped = false;
-				
+
 			}
-			
+
 		}
-		
+
 		numberOfFlippedCards = 0;
-		
+
 	}
-	
+
 	void DoMatchEffect(CardState cardState){
-		
+
 		switch(cardState.cardType){
-			
+
 			case(CardState.CARD_TYPE.NULL):{
-				
+
 				//null: do nothing
 				break;
-				
+
 			}
 			case(CardState.CARD_TYPE.WIND):{
-				
+
 				//wind: shuffle cards
 				cardManager.shuffleCards();
 				break;
-				
+
 			}
-			
+
 		};
-		
+
 	}
-	
+
 	void DoAllMatchEffects(){
-		
+
 		//process match effects of cards
-		
+
 		CardState[] cardQueue = new CardState[numberOfFlippedCards];
 		System.Array.Copy(flippedCards,cardQueue,numberOfFlippedCards);
-		
+
 		for(int i = 0; i < numberOfFlippedCards; i++){
-			
+
 			if(cardQueue[i] == null) continue;
-			
+
 			if(cardQueue[i].matched){
-				
+
 				//we only process each effect once, regardless of how many cards we flipped.
 				//so remove all the cardStates from further down the queue that have the same type as the one that was just matched.
-				
+
 				for(int j = i+1; j < numberOfFlippedCards; j++){
-					
+
 					if(cardQueue[i].cardType == cardQueue[j].cardType){
-						
+
 						cardQueue[j] = null;
-						
+
 					}
-					
+
 				}
-				
+
 				//then, process the match effect
-				
+
 				DoMatchEffect(cardQueue[i]);
-				
+
 			}
-			
+
 		}
-		
+
 	}
-	
+
     void CheckMatch()
     {
-        
+
 		//checks for a match.
 		//removes the matching cards if a match is found, and clears flipped cards if we've selected two without a match.
-		
+
 		bool matched = false;
-		
+
 		for(int i = 0; i < numberOfFlippedCards; i++){
-			
+
 			for(int j = i+1; j < numberOfFlippedCards; j++){
-				
+
 				if(flippedCards[i].IsMatch(flippedCards[j])){
-					
+
 					matched = true;
-					
+          //ps1.play();
+
 					flippedCards[i].matched = true;
 					flippedCards[j].matched = true;
-					
+
 				}
-				
+
 			}
-			
+
 		}
-		
+
 		DoAllMatchEffects();
-		
+
 		cardManager.updateAllVisuals();
 
 		if(matched){
-			
+
 			UnflipAllCards();
-			
+
 		}else if(numberOfFlippedCards >= 2){
-			
+
 			UnflipAllCards();
-			
+
 		}
-		
+
     }
-	
+
 }
